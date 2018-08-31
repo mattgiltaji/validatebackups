8b82f1d69ccdb5b775e27d8fb4ad5212797f701b
Matt Giltaji <matt@giltaji.com>
2018-08-30 23:29:26 -0700

    add functionality to main to save json file, needs to be refactored out and get tests

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/option"
@@ -27,17 +29,23 @@ func main() {
 	logFatalIfErr(err, "Unable to connect to google cloud storage.")
 
 	//loop over relevant buckets for validating
+	//TODO: refactor into validateBucketsInConfig method
+	//TODO: move to validateBackups and add test coverage
 	for _, bucketConfig := range config.Buckets {
 		bucket := client.Bucket(bucketConfig.Name)
 		//validate the bucket, if the type merits it
 		err = validateBucket(bucket, ctx, config)
 		logFatalIfErr(err, fmt.Sprintf("Bucket %s failed validation.", bucketConfig.Name))
 
-		fmt.Printf("Bucket %s has passed validation", bucketConfig.Name)
+		fmt.Println(fmt.Sprintf("Bucket %s has passed validation.", bucketConfig.Name))
 	}
 
 	//now see if we have files to download already
+	//TODO: add check for in progress file
+
 	//if not, make that file
+	//TODO: refactor into getObjectsToDownloadFromBucketsInConfig method
+	//TODO: move to validateBackups and add test coverage
 	bucketToFilesMapping := make([]BucketAndFiles, len(config.Buckets))
 	for i, bucketConfig := range config.Buckets {
 		bucket := client.Bucket(bucketConfig.Name)
@@ -47,10 +55,23 @@ func main() {
 		bucketToFilesMapping[i] = BucketAndFiles{BucketName: bucketConfig.Name, Files: files}
 	}
 	//serialize bucketToFilesMapping to json file
+	saveInProgressFile(bucketToFilesMapping)
 
 	//now go over the file contents and download the objects locally
 	//ideally give some progress indicator : downloading X/Y files for bucket X
 	return
+}
+
+func saveInProgressFile(data []BucketAndFiles) {
+	//TODO: clean up error handling to do annotate calls and raise error up chain
+	//TODO: move to validateBackups and add test coverage
+	jsonData, err := json.Marshal(data)
+	logFatalIfErr(err, "Unable to marshal file mapping to json")
+	jsonFile, err := os.Create("./downloadsInProgress.json")
+	logFatalIfErr(err, "Unable to open downloadsInProgress file for saving data.")
+	defer jsonFile.Close()
+	_, err = jsonFile.Write(jsonData)
+	logFatalIfErr(err, "Unable to save data to downloadsInProgress file.")
 }
 
 func logFatalIfErr(err error, msg string) {
